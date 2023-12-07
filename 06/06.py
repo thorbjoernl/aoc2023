404af38607bcd9346a8659c5ea0b6212902f7330
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2023-12-07 14:14:09 +0100

    Rewrite Day 6 to use quadratic equation.

--- 06/06.py
+++ 06/06.py
@@ -1,3 +1,4 @@
+import math
 with open("input.txt", "r") as f:
     lines = f.readlines()
 
@@ -5,22 +6,20 @@ times = [x for x in map(lambda x : int(x.strip()), list(filter(None, lines[0].sp
 
 record_distances = [x for x in map(lambda x : int(x.strip()), list(filter(None, lines[1].split(" ")[1:])))]
 
+def quadratic_equation(a, b, c):
+    x1 = (-b + math.sqrt(b**2-(4*a*c)))/(2*a)
+    x2 = (-b - math.sqrt(b**2-(4*a*c)))/(2*a)
+    return x1, x2
 
-print(times)
-print(record_distances)
-
-# Part 1
 product = 1
+for duration, distance in zip(times, record_distances):
+    a = -1
+    b = duration
+    c = -distance
+    bound1, bound2 = quadratic_equation(a, b, c)
 
-for time, record in zip(times, record_distances):
-    count = 0
-    for hold_duration in range(0, time):
-        distance = (time-hold_duration)*hold_duration
-
-        if distance > record:
-            count = count +1
-
-    product = product * count
+    print(bound1, bound2)
+    product = product*abs(int(bound1)-int(bound2))
 
 print(f"Part 1 solution: {product}")
 
@@ -28,14 +27,10 @@ print(f"Part 1 solution: {product}")
 time = int("".join([str(x) for x in times]))
 record = int("".join([str(x) for x in record_distances]))
 
-print(time, distance)
+a = -1
+b = time
+c = -record
+bound1, bound2 = quadratic_equation(a, b, c)
 
-count = 0
-for hold_duration in range(0, time):
-    distance = (time-hold_duration)*hold_duration
+print(f"Part 2 solution: {abs(int(bound1)-int(bound2))}")
 
-    if distance > record:
-        count = count +1
-
-
-print(f"Part 2 solution: {count}")
