295f2caf4603be80e1fc8eb3ec6aa33efdf6b49b
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2023-12-10 12:33:29 +0100

    Add Day 9 - Part 2

--- 09/09.py
+++ 09/09.py
@@ -30,14 +30,28 @@ def predict_next_value(s):
         seq[i].append(seq[i][-1] + seq[i+1][-1])
 
     return seq[0][-1]
-        
+
 sequences = load_configuration("input.txt")
 
 # Part 1
-_sum = 0
-for s in sequences:
-    _sum = _sum + predict_next_value(s)
+#_sum = 0
+#for s in sequences:
+#   _sum = _sum + predict_next_value(s)
 
-print(f"Solution - Part 1: {_sum}")
+print(f"Solution - Part 1: {sum(map(predict_next_value, sequences))}")
 
 # Part 2
+def predict_previous_value(s):
+    seq = [s]
+    while True:
+        seq.append(produce_next_sequence(seq[-1]))
+
+        if all([(x == 0) for x in seq[-1]]):
+            break
+ 
+    for i in reversed(range(0, len(seq)-1)):
+        seq[i].insert(0, seq[i][0] - seq[i+1][0])
+
+    return seq[0][0]
+
+print(f"Solution - Part 2: {sum(map(predict_previous_value, sequences))}")
